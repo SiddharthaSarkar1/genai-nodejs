7aeb8ff24770d43aae1690f8efde3aed3eeada0b
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-11-13 01:49:22 +0530

    image generation openai

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { OpenAI } from "openai";
 import dotenv from "dotenv";
+import { writeFileSync } from "fs";
 
 dotenv.config();
 
@@ -7,32 +8,23 @@ const APIKEY = process.env.OPENAI_KEY;
 
 const client = new OpenAI({ apiKey: APIKEY });
 
-const context = [
-  {
-    role: "system",
-    content: "keep answer short and simple",
-  },
-];
-
-async function aiAnswer(promptByUser) {
-  context.push({ role: "user", content: promptByUser });
-  const response = await client.responses.create({
-    model: "gpt-4o-mini",
-    input: context,
+async function main() {
+  const response = await client.images.generate({
+    model: "dall-e-2",
+    prompt: "a cute baby boy in a bus",
+    size: "512x512",
+    response_format: "b64_json",
+    n:1,
   });
-  //adding output to the context
-  context.push({ role: "assistant", content: response.output_text });
-  console.log(response.output_text);
-}
-// aiAnswer();
 
-process.stdout.write("Ask your question?");
-process.stdin.on("data", (data) => {
-  //   console.log(data.toString().trim());
-  const question = data.toString().trim();
-  if (question.toLowerCase() == "exit") {
-    process.exit();
-  } else {
-    aiAnswer(question);
-  }
-});
+  console.log(response);
+  const rawImage = response.data[0]?.b64_json;
+
+  const path = "./generatedImages/generatedImg.png";
+  const buffer = Buffer.from(rawImage, "base64");
+
+  writeFileSync(path, buffer);
+  console.log(`Image is saved and the path is ${path}.`);
+}
+
+main();
