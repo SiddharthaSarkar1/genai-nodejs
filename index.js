17422e6aad50b5dd2eea8ffd408b91579801262d
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-11-13 01:24:37 +0530

    Memories: contexts in chatbot

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ const APIKEY = process.env.OPENAI_KEY;
 
 const client = new OpenAI({ apiKey: APIKEY });
 
+const context = [
+  {
+    role: "system",
+    content: "keep answer short and simple",
+  },
+];
+
 async function aiAnswer(promptByUser) {
+  context.push({ role: "user", content: promptByUser });
   const response = await client.responses.create({
     model: "gpt-4o-mini",
-    input: promptByUser,
+    input: context,
   });
+  //adding output to the context
+  context.push({ role: "assistant", content: response.output_text });
   console.log(response.output_text);
 }
 // aiAnswer();
