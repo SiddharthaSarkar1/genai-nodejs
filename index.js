67bf28220aa9d91e3dc60b8f2aff54de44866c60
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-11-14 02:17:58 +0530

    gemini basic config param

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ const googleAI = new GoogleGenAI({ apiKey: APIKEY });
 async function main() {
   const response = await googleAI.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Tell table of 54",
+    contents: "How can I be an AI engineer?",
+    config: {
+      // systemInstruction: "give the answer in 20 to 25 words only"
+      // thinkingConfig: {
+      //   includeThoughts: true,
+      //   thinkingBudget: 100
+      // },
+      temperature: 2.0
+    },
   });
 
   console.log(response.text);
+  // console.log(response.candidates[0].content);
 }
 
 main();
